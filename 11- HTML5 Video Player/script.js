fa8bd27ee9b50d5ac77ea21ac21989fafdf14be8
ayninur <nuralaynabdulqadir@gmail.com>
2020-11-30 02:57:28 -0500

    start and stop button toggling, skip ahead button logging to console

--- 11- HTML5 Video Player/script.js
+++ 11- HTML5 Video Player/script.js
@@ -1,16 +1,21 @@
+////////////////////////////////////////////////
 // Get our elements - select them from the page
+////////////////////////////////////////////////
+
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
+// anything that has a data-skip attribute
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
+////////////////////////////////////////////////
 // Build out functions
-
+////////////////////////////////////////////////
 // toggles the play/pause
-const togglePlay = () => {
+function togglePlay() {
     // paused is a property that lives on the video
     if (video.paused) {
         video.play();
@@ -19,15 +24,23 @@ const togglePlay = () => {
     }
 }
 
-// toggle the button play/pause icone
-function toggleButton() {
-    const icon = this.paused ? '►' : '❚ ❚';
-    console.log(icon);
+//toggles play/pause button when played/paused
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚'
     toggle.textContent = icon;
 }
 
+//skips
+function skip() {
+    console.log('skipping')
+}
+
+
 // hook up the event listeners 
-video.addEventListener('click', togglePlay)
-video.addEventListener('click', togglePlay)
-video.addEventListener('play', toggleButton)
-video.addEventListener('pause', toggleButton)
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+//bc there's more than one
+skipButtons.forEach(button => button.addEventListener('click', skip))
+
