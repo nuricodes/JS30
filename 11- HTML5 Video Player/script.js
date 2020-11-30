2a58bf4f5ba5b217b17bb2dbe2d9823a8c7fbd78
ayninur <nuralaynabdulqadir@gmail.com>
2020-11-30 03:37:30 -0500

    all controls work except drag

--- 11- HTML5 Video Player/script.js
+++ 11- HTML5 Video Player/script.js
@@ -30,9 +30,34 @@ function updateButton() {
     toggle.textContent = icon;
 }
 
-//skips
+//skip ahead and behind
 function skip() {
-    console.log('skipping')
+    //name of attribute and property of attribute in dot .
+    console.log(this.dataset.skip);
+    video.currentTime += +this.dataset.skip
+}
+
+//range
+function handleRangeUpdate() {
+    //makes the value of the video equal to the value of where the mouse is on vid
+    video[this.name] = this.value;
+    // console.log(this.value);
+    // console.log(this.name);
+    // console.log(video[this.name])
+}
+
+//update the value of the progress bar
+function handleProgress() {
+    //update width of progress bar
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub(e) {
+    //where you click on progress / width of it
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    //when clicked update video time
+    video.currentTime = scrubTime;
 }
 
 
@@ -40,7 +65,17 @@ function skip() {
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 //bc there's more than one
-skipButtons.forEach(button => button.addEventListener('click', skip))
-
+skipButtons.forEach(button => button.addEventListener('click', skip));
+// for when we drag updates at stop
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+//updates while being dragged
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+let mousedown = false;
+progressBar.addEventListener('click', scrub);
+progressBar.addEventListener('mousemove', scrub);
+progressBar.addEventListener('mousedown', () => mousedown);
+progressBar.addEventListener('mouseup', scrub);
+// (21.50)
